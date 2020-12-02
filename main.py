1dbef116baf84436c100f3268eec8a282053606b
Krittika-Saha <krittika.saha.dev@gmail.com>
2020-12-02 16:27:32 +0530

    Updates and minor bug fix
    I added 2 more function to the power and square_root. Made int to float

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #---------------------Calculator----------------------------
 from art import logo
 from os import system
+from math import sqrt
 
 def add(n1, n2):
   """Adds 2 numbers"""
@@ -17,43 +18,60 @@ def multiply(n1, n2):
 def divide(n1 , n2):
    """Divides 2 numbers"""
    return n1/n2
-  
+def square(n1, n2):
+  """Squares n1 by n2"""
+  return n1**n2
 operations = {
   "+": add,
   '-': substract,
   '*': multiply,
-  "/": divide
+  "/": divide,
+  "**": square,
 }
-print(logo)
+
 def calculator():
-  num1 = int(input("What is the first number?: "))
-  num2 = int(input("What is the second number?: "))
+  """Calculator"""
+  print(logo)
+  num1 = float(input("What is the first number?: "))
   for symbol in operations:
     print(symbol)
+  print("sqrt")
   operation_symbol = input("Pick an operation from the value above ☝: ")
-  calculation_function = operations[operation_symbol]
-  first_answer = calculation_function(num1, num2)
+  if operation_symbol == 'sqrt':
+    first_answer = sqrt(num1)
+    print(f"√{num1} = {first_answer}")
 
-  print(f"{num1} {operation_symbol} {num2} = {first_answer}")
-  want_to_exit = input("Type 'y' to continue chaining or 'n' to exit:")
+  else:
+    num2 = float(input("What is the second number?: "))
+    calculation_function = operations[operation_symbol]
+    first_answer = calculation_function(num1, num2)
+    print(f"{num1} {operation_symbol} {num2} = {first_answer}")
+  want_to_exit = input("Type 'y' to continue chaining or 'n' to start a new calculation or 'exit' to exit:")
   if want_to_exit == 'y':
     pass
+  elif want_to_exit == 'n':
+    system('CLS')
+    calculator()
   else:
     print("Bye!")
     exit()
   while True:
     operation_symbol = input("Pick another operation: ")
-    num3 = int(input("What is the next number?: "))
+    num3 = float(input("What is the next number?: "))
     calculation_function = operations[operation_symbol]
     second_answer = calculation_function(first_answer, num3)
 
     print(f"{first_answer} {operation_symbol} {num3} = {second_answer}")
 
-    want_to_exit2 = input("Type 'y' to continue chaining or 'n' to start a new calculation:")
+    want_to_exit2 = input("Type 'y' to continue chaining or 'n' to start a new calculation or 'exit' to exit:")
     if want_to_exit2 == 'y':
       first_answer=second_answer
-    else:
+    elif want_to_exit2 == 'n':
       system('CLS')
       calculator()
+    else:
+      print("Bye!")
+      exit()
+
 
 calculator()
